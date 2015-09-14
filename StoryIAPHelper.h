7b40c8085c376b0fef6d82872adc6c5e03f0f788
Agust Rafnsson <agustr@gmail.com>
2015-09-14 10:04:12 +0200

    Rey's store kit adapted to the projects needs apart from scalability. The story bundle names are hardcoded into the StoryIAPHelper.

---
+++ StoryIAPHelper.h
@@ -0,0 +1,15 @@
+//
+//  storyIAPHelper.h
+//  GullKistan
+//
+//  Created by AGUST RAFNSSON on 11/21/12.
+//
+//
+
+#import "IAPHelper.h"
+
+@interface StoryIAPHelper : IAPHelper
+
++ (StoryIAPHelper *) sharedInstance;
+
+@end
