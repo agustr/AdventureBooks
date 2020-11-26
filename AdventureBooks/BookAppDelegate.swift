15b358f876f35c5ccb496b5be0f5b3e04f7e061a
Agust Rafnsson <agust@urbit.com>
2020-11-26 10:47:21 +0100

    Migrated the app delegate to swift and enabled background audio playing.

---
+++ AdventureBooks/BookAppDelegate.swift
@@ -0,0 +1,21 @@
+import AVFoundation
+import UIKit
+
+@UIApplicationMain
+class BookAppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool
+    {
+        UserDefaults.standard.register(defaults: ["showText": true])
+        UserDefaults.standard.register(defaults: ["autoPageTurning": true])
+        UserDefaults.standard.register(defaults: ["playAudio": true])
+
+        do {
+            try AVAudioSession.sharedInstance().setCategory(.playback)
+        } catch {
+            print("could not set audio category option due to: \(error)")
+        }
+        return true
+    }
+}
